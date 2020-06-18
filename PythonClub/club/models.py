af5d290d3e90958d2dc3853495b827260fc13a3c
mmh <mmhansen@gmail.com>
2020-06-18 10:33:58 +0000

    Adds to club/models.py

--- PythonClub/club/models.py
+++ PythonClub/club/models.py
@@ -3,4 +3,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Meeting(models.Model):
+	meetingTitle
+	meetingDate
+	meetingTime
+	meetingLocation
+	meetingAgenda
 
+class MeetingMinutes
+	meetingId
+	meetingTime
+	meetingLocation
+	meetingAgenda
+
+class Resource
+	resourceName
+	resourceType
+	resourceURL
+	resourceDateEntered
+	userId
+	resourceDescription
+
+class Event
+	eventTitle
+	eventLocation
+	eventDate
+	eventTime
+	eventDescription
+	userId	
