2cc744f30ac0aea2ed874789b8e3cecae257e031
mmh <mmhansen@gmail.com>
2020-06-19 04:20:57 +0000

    models.py OK on migrate

--- PythonClub/club/models.py
+++ PythonClub/club/models.py
@@ -7,28 +7,43 @@ class Meeting(models.Model):
 	meetingTitle=models.CharField(max_length=255)
 	meetingDate=models.DateField()
 	meetingTime=models.TimeField()
-	meetingLocation=
+	meetingLocation=models.CharField(max_length=255)
 	meetingAgenda=models.CharField(max_length=255)
+	
+	def __str__(self):
+		return self.meetingTitle
+
+	class Meta:
+		db_table='meeting'
 
 class MeetingMinutes(models.Model):
-	meetingId=
+	meetingId=models.IntegerField(primary_key=True)
 	meetingTime=models.TimeField()
-	meetingLocation=
+	meetingLocation=models.CharField(max_length=255)
 	meetingAgenda=models.CharField(max_length=255)
 
+	class Meta:
+		db_table='meetingminutes'
+
 class Resource(models.Model):
-	resourceName=
-	resourceType=
-	resourceURL=
+	resourceName=models.CharField(max_length=255)
+	resourceType=models.TextField()
+	resourceURL=models.URLField()
 	resourceDateEntered=models.DateField()
 	userId=models.CharField(max_length=255)
 	resourceDescription=models.CharField(max_length=255)
 
+	class Meta:
+		db_table="resource"
+
 class Event(models.Model):
 	eventTitle=models.CharField(max_length=255)
-	eventLocation=
+	eventLocation=models.CharField(max_length=255)
 	eventDate=models.DateField()
 	eventTime=models.TimeField()
 	eventDescription=models.CharField(max_length=255)
 	userId=models.CharField(max_length=255)
+
+	class Meta:
+		db_table='event'
 
