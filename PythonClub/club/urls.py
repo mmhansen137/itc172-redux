ef56e831225a1684238880df2eeb0243723f3c76
mmh <mmhansen@gmail.com>
2020-06-20 03:57:31 +0000

    Adds resources view to club/urls.py

--- PythonClub/club/urls.py
+++ PythonClub/club/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
         path('', views.index, name='index'),
 	path('getmeetings/', views.getmeetings, name='meetings'),
+	path('getresourses/', views.getresourses, name='resources'),
 ]
 
