136bc7a888310be223d8597f5bed825309c8d197
mmh <mmhansen@gmail.com>
2020-06-20 04:58:27 +0000

    Fixes error preventing resorces from displaying.

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -10,7 +10,15 @@ def getmeetings(request):
 	meeting_list=Meeting.objects.all()
 	return render(request, 'club/meetings.html' ,{'meeting_list' : meeting_list})
 
+def getminutes(request):
+	minutes_list=MeetingMinutes.objects.all()
+	return render(request, 'club/minutes.html' ,{'minutes_list' : minutes_list})
+
 def getresources(request):
 	resource_list=Resource.objects.all()
 	return render(request, 'club/resources.html' ,{'resource_list' : resource_list})
+
+def getevents(request):
+	event_list=Event.objects.all()
+	return render(request, 'club/event.html' , {'event_list' : event_list})
 
