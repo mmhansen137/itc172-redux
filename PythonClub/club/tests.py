9344a2345e204331603ddafd72295da54faf9153
mmh <mmhansen@gmail.com>
2020-06-22 09:41:10 +0000

    First test case for Meetings model.

--- PythonClub/club/tests.py
+++ PythonClub/club/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import Meeting, Meeting, MeetingMinutes, Resource, Event
 # Create your tests here.
+
+class MeetingClassTest(TestCase):
+	def test_string(self):
+		meetTest=Meeting(meetingTitle="Test Meeting")
+		self.assertEqual(str(meetTest), meetTest.meetingTitle)
+
+	def test_table(self):
+		self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
