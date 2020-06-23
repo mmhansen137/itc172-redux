35eac0b1848625aa2dae9470baa54cafddc47a9c
mmh <mmhansen@gmail.com>
2020-06-23 14:51:11 +0000

    Event form displays, but is broken

--- PythonClub/club/views.py
+++ PythonClub/club/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
-from .forms import MeetingForm
+from .forms import MeetingForm, EventForm
 
 # Create your views here.
 
